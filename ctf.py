12c890d98aa4c775d9484a7cb9db399e63f8098c
kirbix <kirbix.dev@proton.me>
2026-03-05 13:19:05 +0100

    Implement CTF lab script with VPN connection options

--- ctf.py
+++ ctf.py
@@ -1,1 +1,21 @@
-# soon
+#!/usr/bin/env python3
+from colorama import Fore, Back, Style, init
+import os
+import getpass
+
+
+init()
+user = getpass.getuser()
+os.system("clear")
+print(Fore.RED + f"ctf lab {user} | code by ovax  1.0")
+print()
+print()
+
+x = str(input(Style.BRIGHT + "CTF | HTB or THM ?: "))
+if x == "HTB":
+    print('\033[3B'+"CTF HTB ")
+    os.system("openvpn your path vpn here") # your vpn Hack the box
+
+elif x == "THM":
+    print('\033[3B'+"CTF THM ")
+    os.system("openvpn your path vpn here") # your vpn Try hack me
